85cf1bb7ecd149b1f6dfdb91f7ed234d3525a9ae
Chris Lee <christopher.lee222@yahoo.com>
2022-05-25 23:53:01 -0500

    start react app

---
+++ client/src/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders 3 list itemsk', () => {
+  render(<App />);
+  const listItems = screen.getAllByRole("listitem");
+  // expect(listItems).toHaveLength(3);
+  expect(listItems.length).toBe(3);
+
+});
